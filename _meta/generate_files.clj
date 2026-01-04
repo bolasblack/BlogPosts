ccde16685b548c61c9354c252221733356a456c4
c4605 <bolasblack@gmail.com>
2026-01-04 20:13:57 +0100

    将 generate_feed.clj 改为 generate_files.clj，添加分页 JSON 生成功能
    - 重命名脚本为 generate_files.clj
    - 添加分页 JSON 文件生成（每页 10 篇）
    - index.json 包含元数据（总页数、总文章数）和第一页数据
    - page-N.json 包含后续页数据
    - 删除 pre-commit hook（改用 GitHub Action）
    - 更新 GitHub Action 使用新脚本

---
+++ _meta/generate_files.clj
@@ -0,0 +1,205 @@
+#!/usr/bin/env bb
+
+(ns generate-files
+  (:require [babashka.fs :as fs]
+            [babashka.process :refer [shell sh]]
+            [clojure.string :as str]
+            [clojure.java.io :as io]
+            [selmer.parser :as selmer]
+            [clj-yaml.core :as yaml]
+            [cheshire.core :as json])
+  (:import [java.time ZonedDateTime LocalDate ZoneId]
+           [java.time.format DateTimeFormatter]))
+
+(def script-dir
+  (if *file*
+    (fs/parent *file*)
+    (System/getProperty "user.dir")))
+
+(def repo-dir (fs/parent script-dir))
+(def posts-per-page 10)
+
+(defn read-settings []
+  (let [settings (yaml/parse-string (slurp (fs/file script-dir "settings.yml")))]
+    {:extname (get settings :extname ".md")
+     :github-repo (get settings :github_repo)
+     :url (get settings :url)
+     :title (get settings :title)
+     :description (get settings :description "")
+     :lang (get settings :lang)
+     :author-name (get settings :author_name)
+     :author-email (get settings :author_email)
+     :author-uri (get settings :author_uri)}))
+
+(defn meta-url [path]
+  (let [settings (read-settings)]
+    (str "https://github.com/" (:github-repo settings) "/blob/master/_meta/" path)))
+
+(defn post-url [path]
+  (let [settings (read-settings)]
+    (str "https://github.com/" (:github-repo settings) "/blob/master/" path)))
+
+(defn parse-date-from-filename [filename]
+  (when-let [[_ date-str] (re-find #"^(\d{4}-\d{2}-\d{2})-" filename)]
+    date-str))
+
+(defn git-file-dates [file-path]
+  (let [result (sh {:dir (str repo-dir)} "git" "log" "--follow" "--format=%aI" "--" (str file-path))
+        dates (when (zero? (:exit result))
+                (->> (str/split-lines (:out result))
+                     (filter (complement str/blank?))))]
+    {:created-at (last dates)
+     :updated-at (first dates)}))
+
+(defn format-date [date-str]
+  (when date-str
+    (try
+      (let [zdt (ZonedDateTime/parse date-str)]
+        (.format zdt DateTimeFormatter/ISO_OFFSET_DATE_TIME))
+      (catch Exception _
+        date-str))))
+
+(defn date-from-filename [filename]
+  (when-let [date-str (parse-date-from-filename filename)]
+    (let [local-date (LocalDate/parse date-str)
+          zdt (.atStartOfDay local-date (ZoneId/systemDefault))]
+      (.format zdt DateTimeFormatter/ISO_OFFSET_DATE_TIME))))
+
+(defn parse-frontmatter [content]
+  (let [parts (str/split content #"(?m)^---\s*$" 3)]
+    (if (and (>= (count parts) 3) (str/blank? (first parts)))
+      (let [yaml-str (second parts)
+            body (nth parts 2)]
+        {:meta (yaml/parse-string yaml-str)
+         :content (str/trim body)})
+      {:meta {}
+       :content (str/trim content)})))
+
+(defn markdown-to-html [md-content]
+  (let [result (sh {:in md-content} "pandoc" "-f" "gfm" "-t" "html")]
+    (if (zero? (:exit result))
+      (:out result)
+      md-content)))
+
+(defn get-post-metadata [file-path]
+  "Get post metadata without content (for JSON generation)"
+  (let [settings (read-settings)
+        filename (fs/file-name file-path)
+        extname (:extname settings)
+        basename (str/replace filename (re-pattern (str extname "$")) "")
+        content (slurp file-path)
+        {:keys [meta]} (parse-frontmatter content)
+        git-dates (git-file-dates file-path)
+        default-date (date-from-filename filename)
+        date-str (parse-date-from-filename filename)
+        title (or (:title meta)
+                  (-> basename
+                      (str/replace #"^\d{4}-\d{2}-\d{2}-" "")
+                      (str/replace "_" " ")))]
+    {:id basename
+     :path filename
+     :title title
+     :date date-str
+     :created-at (or (format-date (:created-at git-dates)) default-date)
+     :updated-at (or (format-date (:updated-at git-dates)) default-date)
+     :category (first (:category meta))
+     :tags (or (:tags meta) [])
+     :url (post-url filename)}))
+
+(defn get-post-with-content [file-path]
+  "Get post with HTML content (for feed generation)"
+  (let [metadata (get-post-metadata file-path)
+        content (slurp file-path)
+        {:keys [content]} (parse-frontmatter content)
+        html-content (markdown-to-html content)]
+    (assoc metadata
+           :content html-content
+           :post-url (:url metadata)
+           :id-url (post-url (:id metadata)))))
+
+(defn get-all-posts []
+  "Get all posts sorted by updated-at (newest first)"
+  (let [settings (read-settings)
+        extname (:extname settings)
+        md-files (->> (fs/glob repo-dir (str "*" extname))
+                      (map str)
+                      (filter #(re-find #"^\d{4}-\d{2}-\d{2}-" (fs/file-name %))))]
+    (->> md-files
+         (map get-post-metadata)
+         (sort-by :updated-at)
+         reverse)))
+
+(defn get-posts-for-feed []
+  "Get posts with content for feed (limited to 10)"
+  (let [settings (read-settings)
+        extname (:extname settings)
+        md-files (->> (fs/glob repo-dir (str "*" extname))
+                      (map str)
+                      (filter #(re-find #"^\d{4}-\d{2}-\d{2}-" (fs/file-name %))))]
+    (->> md-files
+         (map get-post-with-content)
+         (sort-by :updated-at)
+         reverse
+         (take 10))))
+
+(defn paginate [items per-page]
+  "Paginate items into groups"
+  (partition-all per-page items))
+
+(defn page-filename [page-num]
+  (if (= page-num 1)
+    "index.json"
+    (str "page-" page-num ".json")))
+
+(defn generate-posts-json []
+  "Generate paginated JSON files for posts"
+  (let [posts (get-all-posts)
+        total-posts (count posts)
+        pages (paginate posts posts-per-page)
+        total-pages (count pages)
+        data-dir (fs/file script-dir "data")]
+    ;; Create data directory if not exists
+    (fs/create-dirs data-dir)
+    ;; Generate each page
+    (doseq [[page-idx page-posts] (map-indexed vector pages)]
+      (let [page-num (inc page-idx)
+            is-first (= page-num 1)
+            is-last (= page-num total-pages)
+            page-data (cond-> {:pagination {:current-page page-num
+                                            :has-prev (not is-first)
+                                            :has-next (not is-last)
+                                            :prev-file (when (not is-first)
+                                                         (page-filename (dec page-num)))
+                                            :next-file (when (not is-last)
+                                                         (page-filename (inc page-num)))}
+                               :posts (vec page-posts)}
+                        is-first (assoc :meta {:total-posts total-posts
+                                               :total-pages total-pages
+                                               :per-page posts-per-page
+                                               :generated-at (.format (ZonedDateTime/now)
+                                                                      DateTimeFormatter/ISO_OFFSET_DATE_TIME)}))
+            filename (page-filename page-num)]
+        (spit (fs/file data-dir filename)
+              (json/generate-string page-data {:pretty true}))))
+    (println (str "Generated " total-pages " JSON files in data/ directory"))))
+
+(defn generate-feed []
+  "Generate Atom feed XML"
+  (let [settings (read-settings)
+        posts (get-posts-for-feed)
+        updated-at (or (:updated-at (first posts))
+                       (.format (ZonedDateTime/now) DateTimeFormatter/ISO_OFFSET_DATE_TIME))
+        template (slurp (fs/file script-dir "feed.xml.selmer"))
+        feed-content (selmer/render template
+                                    {:site (assoc settings :updated-at updated-at)
+                                     :posts posts
+                                     :meta-url (meta-url "feed.xslt.xml")
+                                     :feed-id (str (meta-url "") "/")})]
+    (spit (fs/file script-dir "feed.xml") feed-content)
+    (println "feed.xml generated successfully")))
+
+(defn -main []
+  (generate-feed)
+  (generate-posts-json))
+
+(-main)
